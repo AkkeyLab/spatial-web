8ad671518246bcb90f03a0007eb25fdbddefd88d
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2025-06-17 01:53:26 +0900

    ✨ Suport shutting down server gracefully

--- server.py
+++ server.py
@@ -1,13 +1,26 @@
 import http.server
 import socketserver
 import mimetypes
-
-mimetypes.add_type("model/vnd.usdz+zip", ".usdz")
+import signal
+import sys
+import threading
 
 PORT = 8000
-Handler = http.server.SimpleHTTPRequestHandler
+mimetypes.add_type("model/vnd.usdz+zip", ".usdz")
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+def run_server():
     print(f"Serving at http://localhost:{PORT}")
     httpd.serve_forever()
 
+def shutdown_server(sig, frame):
+    print("\nShutting down server gracefully...")
+    httpd.shutdown()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, shutdown_server)
+
+server_thread = threading.Thread(target=run_server)
+server_thread.start()
